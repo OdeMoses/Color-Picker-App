ea790ab49d900d3de6de2466b94be0dcb273a7fe
Ode_Moses <aodemoses@gmail.com>
2024-08-18 14:28:42 +0100

    Done project

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 function ColorPicker() {
-  return <div>ColorPicker</div>;
+  const [color, setColor] = useState("#FFFFFF");
+
+  function handleColor(e) {
+    setColor(e.target.value);
+  }
+
+  const whiteColor = color == "#000000" ? "#fff" : "#000";
+
+  return (
+    <>
+      <h1>Color Picker App</h1>
+      <div className="color-display" style={{ backgroundColor: color }}>
+        <p className="color-text" style={{ color: whiteColor }}>
+          Selected Color: {color.toLocaleUpperCase()}
+        </p>
+      </div>
+      <label>Select Color: </label>
+      <input type="color" value={color} onChange={handleColor} />
+    </>
+  );
 }
 
 export default ColorPicker;
